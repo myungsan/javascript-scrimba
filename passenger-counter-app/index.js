d01ee34625c175838765fde95208ceba3ab6f1a1
Myungsan Yun <88599454+myungsan@users.noreply.github.com>
2023-05-09 07:37:08 -0700

    230509

---
+++ passenger-counter-app/index.js
@@ -0,0 +1,18 @@
+let countEl = document.getElementById("count-el")
+
+console.log(countEl)
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+    console.log(count)
+}
+
+
+
+function save() {
+    console.log(count)
+}
+
