4782cb83880822dedbef226f6d533b03d65f5cbf
Myungsan Yun <88599454+myungsan@users.noreply.github.com>
2023-05-21 07:32:05 -0700

    210521

--- blackjack-game/index.js
+++ blackjack-game/index.js
@@ -1,12 +1,16 @@
 let firstCard = 6
 let secondCard = 9
-
 let sum = firstCard + secondCard
+let hasBlackJack = false
 
-if (sum < 21) {
+if (sum <= 20) {
     console.log("Do you want to draw a new card?")
 } else if (sum === 21) {
-    console.log("Yya! You got Blackjack!")
-} else if (sum > 21) {
+    console.log("Wohoo! You've got Blackjack!")
+    hasBlackJack = true
+} else {
     console.log("You're out of the game!")
 }
+
+// CASH OUT!
+console.log(hasBlackJack)
